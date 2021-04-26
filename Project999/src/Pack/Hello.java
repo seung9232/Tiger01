ee4bf6e10f5f7680088ebfec2cbfcef8dea87a88
seung <seung@DESKTOP-BJH2GD4>
2021-04-26 21:48:16 +0900

    1

--- Project999/src/Pack/Hello.java
+++ Project999/src/Pack/Hello.java
@@ -11,6 +11,8 @@ public class Hello {
 		// 4. 프로젝트 생성 및 클래스 생성, 실행 확인
 		// 5. 프로젝트 이름 우클릭 - Team - Share Project
 		// 6. 지역 레파지토리 설정(큰 의미는 두지 말자.)
+		// 7. 최종확인
+		// 8. 테스트
 		
 		System.out.println(1);
 
