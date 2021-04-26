6b72cdbd185c84481dbf5a162a78c4fe6ba30671
seung <seung@DESKTOP-BJH2GD4>
2021-04-26 21:35:43 +0900

    1

---
+++ Project999/src/Pack/Hello.java
@@ -0,0 +1,17 @@
+package Pack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ex104)
+		// 1. Git 접속
+		// 2. 로그인
+		// 3. 레파지토리(repository) 생성
+		// 4. 프로젝트 생성 및 클래스 생성, 실행 확인
+		// 5. 프로젝트 이름 우클릭 - Team - Share Project
+		System.out.println(1);
+
+	}
+
+}
